62c45cd9b0438fffe2d1c208d8070d9b7e672246
Nixian001 <no.oxgen1@gmail.com>
2025-08-04 01:08:47 +0100

    Remove dynamic memory allocation test code from main function

--- kernel/kernel.c
+++ kernel/kernel.c
@@ -56,40 +56,5 @@ void main() {
     print_nl();
     print_nl();
 
-    print_string("init_dynamic_mem()\n");
-    print_dynamic_node_size();
-    print_dynamic_mem();
-    print_nl();
-
-    int *ptr1 = alloc(5);
-    print_string("int *ptr1 = alloc(5)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    int *ptr2 = alloc(10);
-    print_string("int *ptr2 = alloc(10)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    mem_free(ptr1);
-    print_string("mem_free(ptr1)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    int *ptr3 = alloc(2);
-    print_string("int *ptr3 = alloc(2)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    mem_free(ptr2);
-    print_string("mem_free(ptr2)\n");
-    print_dynamic_mem();
-    print_nl();
-
-    mem_free(ptr3);
-    print_string("mem_free(ptr3)\n");
-    print_dynamic_mem();
-    print_nl();
-
     print_string("> ");
 }
